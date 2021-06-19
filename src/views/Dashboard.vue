346a6003aead0aed5f97b4b95522ca62305a5c2b
Joshua Ogbonna <devjaykes@gmail.com>
2021-06-19 21:03:38 +0100

    Dashboard logo

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="dashboard">
     <div class="sidebar">
+      <router-link to="/"><i class="fas fa-link brand"></i></router-link>
+      <!-- User Information -->
 
+      <!-- Manage Section -->
+
+
+
+      <!-- Integrations -->
+
+      <!-- Sign Out Action -->
     </div>
     <div class="main__bar">
-
+      
     </div>
   </div>
 </template>
@@ -19,5 +28,16 @@
   .dashboard {
     display: flex;
     align-items: center;
+    height: 100%;
+  }
+  .sidebar {
+    background: #F4F9F9;
+    width: 20%;
+    height: 100%;
+  }
+  .main__bar {
+    width: 80%;
+    height: 100%;
+    
   }
 </style>
