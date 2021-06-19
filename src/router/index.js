ecc005ff41f1c9313b738b5182650721fd77f85d
Joshua Ogbonna <devjaykes@gmail.com>
2021-06-19 16:53:22 +0100

    Dashboard jobs

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import SignUp from '@/views/SignUp'
 import Login from '@/views/Login'
+import Dashboard from '@/views/Dashboard'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
   }
 ]
 
