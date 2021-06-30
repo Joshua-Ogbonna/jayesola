e327f2312e6c6060be510dcd16bf4bf9e012b18b
Joshua Ogbonna <devjaykes@gmail.com>
2021-06-30 16:44:57 +0100

    Email components styled

--- src/components/Emails.vue
+++ src/components/Emails.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <h1>Emails  here</h1>
+    <section class="add__note">
+      <button
+        type="button"
+        class="btn btn-primary"
+        
+      >
+        Create Email
+      </button>
+    </section>
+
+    <section class="if__note">
+      <p>Send emails to a contact from this record.</p>
+    </section>
   </div>
 </template>
 
@@ -11,5 +23,17 @@
 </script>
 
 <style scoped>
-
+  .add__note button {
+  background: rgb(30, 37, 75);
+  color: #fff;
+  padding: 10px 30px;
+  font-size: 11px;
+  border: none;
+  margin-bottom: 20px;
+  border-radius: 3px;
+}
+.if__note {
+  text-align: center;
+  font-size: 15px;
+}
 </style>
