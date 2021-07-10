b69f14b49f0746f3c288c58a115c2ef41411ef2d
Joshua Ogbonna <devjaykes@gmail.com>
2021-07-10 17:45:31 +0100

    Sales feature work

--- src/components/Sales.vue
+++ src/components/Sales.vue
@@ -1,15 +1,30 @@
 <template>
-  <div>
-    <h1>Sales</h1>
+  <div class="sales">
+    <h3>Sales</h3>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
 
 <style scoped>
-
+.sales {
+  margin-top: 30px;
+}
+h3 {
+  font-weight: 800;
+}
 </style>
